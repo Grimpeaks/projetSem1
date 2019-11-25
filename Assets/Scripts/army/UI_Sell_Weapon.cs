b171cc10c39d8aed2865b2501e6d5ed859904204
Grimpeaks <44480134+Grimpeaks@users.noreply.github.com>
2019-11-25 07:40:36 -0500

    Interface selling désactivation autres ui

--- Assets/Scripts/army/UI_Sell_Weapon.cs
+++ Assets/Scripts/army/UI_Sell_Weapon.cs
@@ -21,6 +21,10 @@ public class UI_Sell_Weapon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!RessourceManager.Instance.get_Is_UI_Craft_Active())
+        {
+            RessourceManager.Instance.set_UI_Craft_Active(true);
+        }
 
     }
 
@@ -53,5 +57,6 @@ public class UI_Sell_Weapon : MonoBehaviour
     {
         Canvas c = GetComponentInParent<Canvas>();
         c.gameObject.SetActive(false);
+        RessourceManager.Instance.set_UI_Craft_Active(false);
     }
 }
